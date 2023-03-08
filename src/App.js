046c4af989bd6415db1ab158a2d80022ee418404
koushil-mankali <koushilmankali@gmail.com>
2023-03-08 10:05:41 +0530

    updated netflix logo removed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import './App.css';
-import request from './resources/requestLinks';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Row from './components/Row';
-import Footer from './components/Footer';
+import "./App.css";
+import request from "./resources/requestLinks";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Row from "./components/Row";
+import Footer from "./components/Footer";
 
 let App = () => {
   return (
     <>
-      <div className='app'>
+      <div className="app">
         <Header />
         <Banner />
-        <Row title ={"Netflix Originals"} fetchURL={request.noriginalsTMBD} bigRow/>
-        <Row title ={"Trending Now"} fetchURL={request.trendingTMBD}/>
-        <Row title ={"Top Rated"} fetchURL={request.topratedTMBD}/>
-        <Row title ={"Action Movies"} fetchURL={request.actionmoviesTMBD}/>
-        <Row title ={"Comedy Movies"} fetchURL={request.comedymoviesTMBD}/>
-        <Row title ={"Horror Movies"} fetchURL={request.horrormoviesTMBD}/>
-        <Row title ={"Documentaries"} fetchURL={request.documentariesTMBD}/>
+        <Row
+          title={"Netflix Clone Originals"}
+          fetchURL={request.noriginalsTMBD}
+          bigRow
+        />
+        <Row title={"Trending Now"} fetchURL={request.trendingTMBD} />
+        <Row title={"Top Rated"} fetchURL={request.topratedTMBD} />
+        <Row title={"Action Movies"} fetchURL={request.actionmoviesTMBD} />
+        <Row title={"Comedy Movies"} fetchURL={request.comedymoviesTMBD} />
+        <Row title={"Horror Movies"} fetchURL={request.horrormoviesTMBD} />
+        <Row title={"Documentaries"} fetchURL={request.documentariesTMBD} />
         <Footer />
       </div>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,46 @@
-import {useState, useEffect } from 'react';
-import logo from '../assets/netflix.png';
-import profile from '../assets/profile.png';
-import './Header.css';
+import { useState, useEffect } from "react";
+import logo from "../assets/netflix.png";
+import profile from "../assets/profile.png";
+import "./Header.css";
 
-let Header = () =>{
+let Header = () => {
+  let [navScroll, setNavScroll] = useState(false);
 
-    let [navScroll, setNavScroll]= useState(false);
-
-    useEffect(()=>{
-        window.addEventListener('scroll',()=>{
-            if(window.scrollY > 100){
-                setNavScroll(true);
-            }else{ setNavScroll(false); }
-        });
-        return () =>{
-            window.removeEventListener('scroll',()=>{
-                if(window.scrollY > 100){
-                    setNavScroll(true);
-                }else{ setNavScroll(false); }
-            });
-        };
-    },[]);
-    return <>
-        <div className={`nav ${navScroll && 'navScroll'}`}>
-            <img className='logo' src={logo} alt='logo'/>
-            <div className='hbtns'>
-                <button type='button' className='join_btn'>JOIN NOW</button>
-                <button type='button' className='sign_btn'>SIGN IN</button>
-            </div>
-            {/* <img className='profile' src={profile} alt='profile' /> */}
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setNavScroll(true);
+      } else {
+        setNavScroll(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+          setNavScroll(true);
+        } else {
+          setNavScroll(false);
+        }
+      });
+    };
+  }, []);
+  return (
+    <>
+      <div className={`nav ${navScroll && "navScroll"}`}>
+        {/* <img  src={logo} alt='logo'/> */}
+        <div className="logo">Netflix Clone</div>
+        <div className="hbtns">
+          <button type="button" className="join_btn">
+            JOIN NOW
+          </button>
+          <button type="button" className="sign_btn">
+            SIGN IN
+          </button>
         </div>
+        {/* <img className='profile' src={profile} alt='profile' /> */}
+      </div>
     </>
-
-}
+  );
+};
 
 export default Header;
